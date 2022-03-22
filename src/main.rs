8f504e2a18f223a9c8d22861f647571efcca23f7
Linus Weigand <linusweigand@protonmail.com>
2022-03-22 13:26:02 +0100

    Error Handling

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     let secret_number = rand::thread_rng()
     .gen_range(1..=100);
 
-    println!("The secret number is {}", secret_number);
     loop {
         println!("Please input your guess.");
 
@@ -18,10 +17,12 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-        let guess: u32 = guess
+        let guess: u32 =  match guess
         .trim()
-        .parse()
-        .expect("Please type a number!");
+        .parse() {
+            Ok(num) => num,
+            Err(_) => {println!("Please input a valid number!"); continue },
+        };
 
         println!("You guessed: {}", guess);
 
