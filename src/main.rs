56a525a9435911debe27ac9c6c0d71a54f05d675
Linus Weigand <linusweigand@protonmail.com>
2022-03-22 12:31:39 +0100

    Looping -> infinite trys

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,28 @@ fn main() {
     .gen_range(1..=100);
 
     println!("The secret number is {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+        let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("Please type a number!");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Lower!"),
-        Ordering::Less => println!("Higher!")
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {println!("You win!"); break;},
+            Ordering::Greater => println!("Lower!"),
+            Ordering::Less => println!("Higher!")
+            
+        }
     }
-
-    println!("You guessed: {}", guess);
+    
 }
